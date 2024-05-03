83ac4b655d69ecc427c11bf3ecc906e1f5c362da
Gunter IV <larry.gunter-iv@capgemini.com>
2024-05-03 15:57:44 -0400

    added splash page when nothing is selected

--- src/components/NewChatCard.jsx
+++ src/components/NewChatCard.jsx
@@ -1,12 +1,25 @@
 // imports
+import React, {
+  useState,
+  useEffect,
+} from "react";
 import NewBottomBar from "./NewBottomBar";
-import React from "react";
+import ChatSplash from "./ChatSplash";
+
 
 // Main Component
 function NewChatCard({
     aiHistory,
     userHistory,
 }) {
+
+    const [isEmpty, setIsEmpty] = useState(true);
+
+    useEffect(() => {
+      // Check if aiHistory and userHistory are empty and update isEmpty state
+      setIsEmpty(aiHistory.length === 0 && userHistory.length === 0);
+  }, [aiHistory, userHistory]); // Watch for changes in aiHistory and userHistory arrays
+
     return (
         <div className="flex flex-col h-[100vh] w-[80vw]">
 
@@ -17,22 +30,26 @@ function NewChatCard({
             </div>
 
             {/* Chat Area */}
-            <div className="overflow-auto">
-                {userHistory.map((message, index) => (
-                    <div className="">
-                        <div className="chat chat-end">
-                            <div className="chat-bubble chat-bubble-success">
-                                {message}
+            {isEmpty ? ( // Conditionally render ChatSplash if isEmpty is true
+                <ChatSplash />
+            ) : (
+                <div className="overflow-auto">
+                    {userHistory.map((message, index) => (
+                        <div key={index} className="">
+                            <div className="chat chat-end">
+                                <div className="chat-bubble chat-bubble-success">
+                                    {message}
+                                </div>
+                            </div>
+                            <div className="chat chat-start">
+                                <div className="chat-bubble bg-capVibrantBlue text-white">
+                                    {aiHistory[index]}
+                                </div>
                             </div>
                         </div>
-                        <div className="chat chat-start">
-                            <div className="chat-bubble bg-capVibrantBlue text-white">
-                                {aiHistory[index]}
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+            )}
             
             {/* Bottom Bar */}
             <div className="mt-auto">
