d31446cbfa7984fe6a38045ccd60266fc80d850b
Gunter IV <larry.gunter-iv@capgemini.com>
2024-05-03 12:15:56 -0400

    changed graph size and coloring

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -9,23 +9,6 @@ function Graph() {
 
   const [graphData, setGraphData] = useState({ nodes: [], links: [] });
 
-  //   useEffect(() => {
-  //     axios.get('https://columbiateam1backend.azurewebsites.net/graphdb_display_test?user_email=test%40test.com')
-  //       .then(response => {
-  //         // Transform the data into nodes and links
-  //         const nodes = [], links = [];
-  //         response.data.forEach(doc => {
-  //           nodes.push({ id: doc.document.id, level: 1, size: 25, ...doc.document });
-  //           doc.children.forEach(child => {
-  //             nodes.push({ id: child.id, level: 2, size: 5, ...child });
-  //             links.push({ source: doc.document.id, target: child.id });
-  //           });
-  //         });
-  //         setGraphData({ nodes, links });
-  //       })
-  //       .catch(error => console.error('Error fetching data:', error));
-  //   }, []);
-
   useEffect(() => {
     axios
       .post(
@@ -37,16 +20,6 @@ function Graph() {
           },
         }
       )
-      //   .then((response) => {
-      //     setGraphData({
-      //       nodes: response.data.nodes,
-      //       links: response.data.links.map((link) => ({
-      //         source: link.source,
-      //         target: link.target,
-      //       })),
-      //     });
-      //   })
-      //   .catch((error) => console.error("Error fetching data:", error));
       .then((response) => {
         console.log("Nodes Response: ", response.data.nodes);
         // Transform the data into nodes and links
@@ -76,21 +49,6 @@ function Graph() {
       })
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
-
-  const getNodeColor = (node) => {
-    switch (node.level) {
-      case 1:
-        return "blue";
-      case 2:
-        return "green";
-      default:
-        return "gray";
-    }
-  };
-
-  const getNodeSize = (node) => {
-    return node.size || 1;
-  };
 
   return (
     <ForceGraph2D
